607e284badd0aa0554d0a8dd4bee18a9dc330c1e
enas <enas.gamal@moltaqa.net>
2025-06-22 18:32:31 +0300

    updates

--- error.vue
+++ error.vue
@@ -1,8 +1,14 @@
-<template></template>
+<template>
+</template>
+
 <script setup>
 const router = useRouter();
 const localePath = useLocalePath();
+const error = useError();
+
 onMounted(() => {
-  router.push(localePath("/not_found"));
-})
+  if (error.value && error.value.statusCode === 404) {
+    router.push(localePath("/not_found"));
+  }
+});
 </script>
